9a0904278afdac8d01d6cb38a83499e8aa652ab4
Nandanipatel04 <141405604+Nandanipatel04@users.noreply.github.com>
2023-08-17 14:49:00 +0530

    Add files via upload

---
+++ solutionreadcsv.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import csv
+
+
+# In[8]:
+
+
+with open('c:\\sqlite3\\csv\\hello.txt',"r",errors="ignore")as f:
+    contents=f.read()
+    print(contents)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
